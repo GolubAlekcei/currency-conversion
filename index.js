b9ca09c35f18660aa084406c8fde1c1a352a19f5
Nikolay Hromadskiy <samsungt285v@gmail.com>
2022-09-14 09:26:58 +0300

    v1.1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let btc = prompt('What is Bitcoin price today?',)
 if (btc < 0){
-    alert('Курс не може бути відємний')
+    alert('The currensy rate cannot be negative')
 }else if (btc === null) {
     alert ('Good buy')
 }else {
     let usd = prompt('How much $ do you have?',)
     let result = usd / btc
     if (isNaN(result)||result === Infinity){
-        alert ('false')
+        alert ('False, press F5 and try again')
     }else {
         alert (`You can buy ${result.toFixed(7)} BTC`)
     }
